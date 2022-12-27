d83db5206c9f557bf4da91c8de62956cc2b73d48
Zaprovic <jazaro2001@gmail.com>
2022-12-27 13:59:02 -0500

    .

---
+++ Familia.java
@@ -0,0 +1,26 @@
+public class Familia {
+
+    Animal papa;
+    Animal mama;
+    Animal hijo;
+
+    String genero;
+
+    void imprimir(){
+        
+
+        if(hijo.genero.equals("M")){
+            genero = "Masculino";
+
+        }
+
+        else{
+            genero = "Femenino";
+        }
+
+        System.out.println("Papa: " + papa.nombre + "\n"
+        + "Mama: " + mama.nombre + "\n" + 
+        "Hijo: " + hijo.nombre);
+    }
+    
+}
