1006536b1589e414b8061a8c87551645147ad98c
Zaprovic <jazaro2001@gmail.com>
2022-12-29 17:37:49 -0500

    .

--- Module1/Exercises/Taller1_Java/Main.java
+++ Module1/Exercises/Taller1_Java/Main.java
@@ -5,11 +5,10 @@ public class Main {
     public static void main(String[] args){
 
         //Asientos
-        
-        Asiento asiento1 = new Asiento();
-        Asiento asiento2 = new Asiento();
-        Asiento asiento3 = new Asiento();
-        Asiento asiento4 = new Asiento();
+        Asiento asiento1 = new Asiento(250,12);
+        Asiento asiento2 = new Asiento(350,12);
+        Asiento asiento3 = new Asiento(150,12);
+        Asiento asiento4 = new Asiento(550,12);
 
         asiento1.cambiarColor("amarillo");
         asiento2.cambiarColor("verde");
@@ -18,6 +17,20 @@ public class Main {
 
         Asiento[] asientosLista = {asiento1,asiento2,
                                     asiento3,asiento4};
+
+        
+
+        //Motor
+        Motor motor1 = new Motor(6,"gasolina",12);
+        motor1.asignarTipo("electrico");
+
+
+        //Auto
+        Auto auto1 = new Auto("XC60",75000,asientosLista,
+        "Volvo",motor1,12);
+
+        Auto.cantidadCreados++;
+
 
     }
 
@@ -28,6 +41,12 @@ class Asiento{
     String color;
     int precio;
     int registro;
+
+    Asiento(int precio, int registro){
+        this.precio = precio;
+        this.registro = registro;
+        
+    }
 
     void cambiarColor(String color){
 
@@ -58,6 +77,18 @@ class Auto{
     int registro;
     static int cantidadCreados = 0;
 
+    Auto(String modelo, int precio, Asiento[] asientos,
+    String marca, Motor motor, int registro){
+        this.modelo = modelo;
+        this.precio = precio;
+        this.asientos = asientos;
+        this.marca = marca;
+        this.motor = motor;
+        this.registro = registro;
+
+
+    }
+
     int cantidadAsientos(){
         return this.asientos.length;
     }
@@ -79,6 +110,13 @@ class Motor{
     int numeroCilindros;
     String tipo;
     int registro;
+
+    Motor(int numeroCilindros, String tipo, int registro){
+        this.numeroCilindros = numeroCilindros;
+        this.tipo = tipo;
+        this.registro = registro;
+
+    }
 
     void cambiarRegistro(int registro){
         this.registro = registro;
