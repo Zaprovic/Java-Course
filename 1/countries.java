adf698d5cfce372f24472b178ad274d852c87f66
Zaprovic <jazaro2001@gmail.com>
2022-12-28 22:18:39 -0500

    .

---
+++ 1/countries.java
@@ -0,0 +1,40 @@
+class countries {
+
+    public static class Country{
+        int population;
+        String name;
+        String language;
+
+
+        public String printLanguage(){
+            return "Here in " + name + " we speak " + language;
+        }
+
+        public void printMessage(){
+            System.out.println("I am a citizen of the world");
+
+        }
+
+    }
+
+    public static class France extends Country{
+
+        @Override
+        public void printMessage(){
+            System.out.println("I am a french person");
+        }
+
+
+    }
+
+    public static void main(String[] args){
+
+        Country country1 = new Country();
+        Country country2 = new France();
+
+        country1.printMessage();
+        country2.printMessage();
+
+    }
+
+}
